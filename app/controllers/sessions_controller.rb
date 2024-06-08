8b6126078ff6ed63e993b1286563761ab503abcb
khalidlotfi <khalidlotfi2005@gmail.com>
2024-06-08 10:03:02 +0100

    Authentication Features

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,5 @@
 class SessionsController < ApplicationController
+  include CurrentUserConcern
   def create
     user = User.find_by(email: params["user"]["email"]).try(:authenticate, params["user"]["password"])
 
@@ -12,5 +13,26 @@ class SessionsController < ApplicationController
     else
       render json: {status: 401}
     end
+  end
+
+  def logged_in
+    if @current_user
+      render json: {
+        logged_in: true,
+        user: @current_user
+      }
+    else
+      render json: {
+        logged_in: false
+      }
+    end
+  end
+
+  def logout
+    reset_session
+    render json: {
+      status: '204 No Content',
+      logged_out: true
+    }
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
 
   resources :sessions, only: %i[create]
   resources :registrations, only: %i[create]
+  delete :logout, to: "sessions#logout"
+  get :logged_in, to: "sessions#logged_in"
 
   # Defines the root path route ("/")
   root "home#index"
